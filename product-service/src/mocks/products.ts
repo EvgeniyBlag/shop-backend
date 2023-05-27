6b3bbcb856aa146a1ea86bc67833904ea95da69e
Evgeniy Blagodarov <evgeniy.blagodarov@gmail.com>
2023-05-27 15:14:40 +0300

    Module 7 (#6)
    * Replace dynamodb calls to mocks
    * Add authorization service

--- product-service/src/mocks/products.ts
+++ product-service/src/mocks/products.ts
@@ -1,44 +1,79 @@
-export const products = [
-    {
-        id: 'YP45',
-        description: '88-Key Weighted Digital Piano',
-        title: 'Yamaha P45',
-        price: 400
-    },
-    {
-        id: 'YP515',
-        description: '88-Key Weighted Digital Piano, White',
-        title: 'Yamaha P515',
-        price: 450
-    },
-    {
-        id: 'YNP12',
-        description: '61-Key Lightweight Portable Keyboard, Black (Power Adapter Sold Separately)',
-        title: 'Yamaha NP12',
-        price: 500
-    },
-    {
-        id: 'YPSR-EW310',
-        description: '76-key Portable Keyboard with Power Supply',
-        title: 'Yamaha PSR-EW310',
-        price: 400
-    },
-    {
-        id: 'YPSR-E273',
-        description: '88-Key Weighted Digital Piano',
-        title: 'Yamaha PSR-E273',
-        price: 710
-    },
-    {
-        id: 'YMODX7',
-        description: '76-Key Synthesizer Workstation, Black & FC4A Assignable Piano Sustain Foot Pedal,MultiColored',
-        title: 'Yamaha MODX7',
-        price: 650
-    },
-    {
-        id: 'YDP184',
-        description: 'Arius Series Console Digital Piano with Bench, Dark Rosewood & amaha UDWL01 WiFi USB/MIDI Adapter',
-        title: 'Yamaha YDP184',
-        price: 400
-    },
-];
+export const products = {
+    Items: [
+        {
+            id: 'YP45',
+            description: '88-Key Weighted Digital Piano',
+            title: 'Yamaha P45',
+            price: 400
+        },
+        {
+            id: 'YP515',
+            description: '88-Key Weighted Digital Piano, White',
+            title: 'Yamaha P515',
+            price: 450
+        },
+        {
+            id: 'YNP12',
+            description: '61-Key Lightweight Portable Keyboard, Black (Power Adapter Sold Separately)',
+            title: 'Yamaha NP12',
+            price: 500
+        },
+        {
+            id: 'YPSR-EW310',
+            description: '76-key Portable Keyboard with Power Supply',
+            title: 'Yamaha PSR-EW310',
+            price: 400
+        },
+        {
+            id: 'YPSR-E273',
+            description: '88-Key Weighted Digital Piano',
+            title: 'Yamaha PSR-E273',
+            price: 710
+        },
+        {
+            id: 'YMODX7',
+            description: '76-Key Synthesizer Workstation, Black & FC4A Assignable Piano Sustain Foot Pedal,MultiColored',
+            title: 'Yamaha MODX7',
+            price: 650
+        },
+        {
+            id: 'YDP184',
+            description: 'Arius Series Console Digital Piano with Bench, Dark Rosewood & amaha UDWL01 WiFi USB/MIDI Adapter',
+            title: 'Yamaha YDP184',
+            price: 400
+        },
+    ]
+};
+
+export const stocks = {
+    Items: [
+        {
+            product_id: 'YP45',
+            count: 5
+        },
+        {
+            product_id: 'YP515',
+            count: 5
+        },
+        {
+            product_id: 'YNP12',
+            count: 5
+        },
+        {
+            product_id: 'YPSR-EW310',
+            count: 5
+        },
+        {
+            product_id: 'YPSR-E273',
+            count: 5
+        },
+        {
+            product_id: 'YMODX7',
+            count: 5
+        },
+        {
+            product_id: 'YDP184',
+            count: 5
+        }
+    ]
+};
